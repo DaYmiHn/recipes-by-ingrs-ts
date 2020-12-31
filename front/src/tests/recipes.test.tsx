11759ece3eb3336d673bf622d987d47140ea3e50
Denis Savin <ds@openstart.ru>
2020-12-31 19:50:00 +0300

    add new test

--- front/src/tests/recipes.test.tsx
+++ front/src/tests/recipes.test.tsx
@@ -1,4 +1,6 @@
 import recipes from '../reducers/recipes'
+import {Recipes} from '../screens/main/recipes'
+
 test('RESET_RECIPES', ()=>{
   const initialState:any = [
     {
@@ -19,4 +21,10 @@ test('LOAD_RECIPES', ()=>{
   const newState:any = recipes(initialState, {recipes:[{title:'test123'}], type:'LOAD_RECIPES'})
   expect(newState.length).toBe(2)
   expect(newState[1].title).toBe('test123')
+})
+
+
+test('getCountMissIngr', ()=>{
+  let Recipe = new (Recipes as any);
+  expect(Recipe.getCountMissIngr(1,3)).toEqual('red')
 })
